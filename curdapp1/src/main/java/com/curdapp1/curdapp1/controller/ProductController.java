77c8446b29c8e4473efa6a3acc2d8a6d1bb7c997
wazidu <wazidullahmurad@gmail.com>
2020-04-17 17:56:09 +0600

    curdapi1

--- curdapp1/src/main/java/com/curdapp1/curdapp1/controller/ProductController.java
+++ curdapp1/src/main/java/com/curdapp1/curdapp1/controller/ProductController.java
@@ -29,12 +29,12 @@ public class ProductController {
     {
         return service.getProducts();
     }
-    @GetMapping("/product/{id}")
+    @GetMapping("/productById/{id}")
     public Product findProductById(@PathVariable int id)
     {
         return service.getProductById(id);
     }
-    @GetMapping("/product/{name}")
+    @GetMapping("/productByName/{name}")
     public Product findProductByName(@PathVariable String name)
     {
         return service.getProductByName(name);
