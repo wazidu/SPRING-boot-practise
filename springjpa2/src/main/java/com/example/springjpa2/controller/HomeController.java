fcab1784cd9f173fd8c84c0d6b1f387bbe7fa4d2
Wazid Ullah Murad <wazidullahmurad@gmail.com>
2020-05-03 14:55:02 +0600

    DATA JPA

---
+++ springjpa2/src/main/java/com/example/springjpa2/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.example.springjpa2.controller;
+
+import com.example.springjpa2.entity.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Wazid Ullah Murad on 11/1/2019.
+ */
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String getHome(Model model){
+
+        List<Student> list= new ArrayList<>();
+
+        list.add(new Student("Murad", "ICT","016"));
+        //list
+        list.add(new Student("mamun","ICT","017"));
+        list.add(new Student("manna","ICT","018"));
+        list.add(new Student("ismayel","ICT","019"));
+
+        model.addAttribute("name","Name: Wazid Ullah Murad");
+        model.addAttribute("dept","Dept: ICT");
+        model.addAttribute("phone","phone: 01685222158");
+        model.addAttribute("mylist",list);
+
+        return "home";
+    }
+}
---
+++ springjpa2/src/test/java/com/example/springjpa2/Springjpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springjpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
