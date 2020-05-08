3a5c760a3adc23601649ae06dc516277fbec0f10
wazidu <wazidullahmurad@gmail.com>
2020-05-08 20:19:51 +0600

    Login Example (using ThymeLeaf)

---
+++ LoginExampleThymeleaf/src/main/java/com/example/controller/LoginForm.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+/**
+ * Created by Wazid Ullah Murad on 5/8/2020.
+ */
+public class LoginForm {
+
+    public String username;
+    public String password;
+
+    public LoginForm(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginForm{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
