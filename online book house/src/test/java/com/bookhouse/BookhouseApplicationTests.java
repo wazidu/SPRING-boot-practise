9bb6f4a7ec863ad9eca9ba4c9924e3b21462b2b2
wazidu <wazidullahmurad@gmail.com>
2020-05-15 12:12:41 +0600

    Online BookStore

---
+++ online book house/src/test/java/com/bookhouse/BookhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
