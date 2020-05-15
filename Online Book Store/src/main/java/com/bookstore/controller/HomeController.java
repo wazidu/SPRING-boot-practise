25283ca03dd95ce7b8d46fc01a632fdb108aa79f
wazidu <wazidullahmurad@gmail.com>
2020-05-15 13:26:54 +0600

    Online Book Store (full ecommerce App)

---
+++ Online Book Store/src/main/java/com/bookstore/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Wazid Ullah Murad on 5/15/2020.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model)
+    {
+        return "index";
+    }
+
+
+}
