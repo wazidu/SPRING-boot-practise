5bfdfb4c39854ee854f7f4bf538c23654f204c67
wazidu <wazidullahmurad@gmail.com>
2020-05-29 18:23:07 +0600

    Online Book Store (full ecommerce App)

--- Online Book Store/src/main/java/com/bookstore/domain/User.java
+++ Online Book Store/src/main/java/com/bookstore/domain/User.java
@@ -1,19 +1,27 @@
 package com.bookstore.domain;
 
+import com.bookstore.domain.security.Authority;
+import com.bookstore.domain.security.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 import org.hibernate.annotations.GeneratorType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Wazid Ullah Murad on 5/29/2020.
  */
 @Entity
 @Table(name = "bookstore_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id", nullable = false,updatable = false)
     private int id;
     private String user_name;
@@ -25,6 +33,10 @@ public class User {
     @Column(name = "email", nullable = false,updatable = false)
     private String email;
     private boolean enabled=true;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<UserRole> userRoles= new HashSet<>();
 
 
     public int getId() {
@@ -83,6 +95,39 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorites = new HashSet<>();
+        userRoles.forEach(ur -> authorites.add(new Authority(ur.getRole().getName())));
+
+        return authorites;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
     public boolean isEnabled() {
         return enabled;
     }
@@ -90,4 +135,17 @@ public class User {
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+//    @Override
+//    public boolean isEnabled() {
+//        return enabled;
+//    }
 }
